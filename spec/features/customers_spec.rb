20c44f45a0d2eae806181661d41f8cb3111159cd
Johannes Vetter <jvse@gmx.at>
2012-12-24 05:04:31 +0100

    move the request specs into the features directory. consider using cucumber at all for integration testing

---
+++ spec/features/customers_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'devise/test_helpers'
+describe "Customers" do
+  describe "GET /customers" do
+    it "works! (now write some real specs)" do
+      user = FactoryGirl.create(:user)
+      visit new_user_session_path
+      fill_in "Email", :with => user.email
+      fill_in "Password", :with => user.password
+      click_button "Sign in"
+
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit customers_path
+      page.should have_content('Listing customers')
+    end
+  end
+end
